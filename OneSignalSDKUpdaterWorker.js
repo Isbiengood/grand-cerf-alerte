652be2ca87a99ab200f0e9f4c4c63a02e0f3ff1c
Isbiengood <lawlessmaison@gmail.com>
2026-06-04 11:27:52 +0200

    Add OneSignal SDK updater worker script

---
+++ OneSignalSDKUpdaterWorker.js
@@ -0,0 +1,9 @@
+<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+<script>
+  window.OneSignalDeferred = window.OneSignalDeferred || [];
+  OneSignalDeferred.push(async function(OneSignal) {
+    await OneSignal.init({
+      appId: "92df196d-df7e-49bf-a9d5-ce548979b922",
+    });
+  });
+</script>
